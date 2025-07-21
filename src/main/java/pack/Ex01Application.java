dabe8dbf0534dd77d62900391b32c7ba9c744763
rail888 <belokana@hanmail.net>
2025-07-21 10:06:55 +0900

    07211006

--- src/main/java/pack/Ex01Application.java
+++ src/main/java/pack/Ex01Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Ex01Application {
 	@GetMapping("/")
 	public String hello(){
-		return "Hello World: finish 0933";
+		return "Hello World: finish 1006";
 	}
 
 	public static void main(String[] args) {
