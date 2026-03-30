4485cb98cff1bb50d6dc7c978ded5d9e078da2fd
rail888 <belokana@hanmail.net>
2026-03-30 09:32:25 +0900

    0931

---
+++ src/main/java/pack/Ex01Application.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Ex01Application {
+	@GetMapping("/")
+	public String hello() {
+		System.out.println("Ex01");
+		return "Ex01 Project:Am 09:21";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01Application.class, args);
+	}
+
+}
