e67b83ce285a25cfc11165520ef5906d41e1b97e
rail888 <belokana@hanmail.net>
2026-03-31 10:50:52 +0900

    1050

--- src/main/java/pack/Ex01Application.java
+++ src/main/java/pack/Ex01Application.java
@@ -11,7 +11,7 @@ public class Ex01Application {
 	@GetMapping("/")
 	public String hello() {
 		System.out.println("Ex01");
-		return "Ex01 Project:Pm 15:34";
+		return "Ex01 Project:am 10:50";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(Ex01Application.class, args);
